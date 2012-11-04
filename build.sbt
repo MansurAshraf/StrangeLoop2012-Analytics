984ae6880293cb56c5c8e1b6f184f5464cd868a5
M. Mansur Ashraf <mansoor.ashraf@gmail.com>
2012-11-04 02:02:17 -0600

    updated the project to use Tika. Removed dependency on dispatch

--- build.sbt
+++
@@ -1,9 +0,0 @@
-
-scalaVersion :="2.9.2"
-
-seq(webSettings :_*)
-
-libraryDependencies ++= Seq("org.mortbay.jetty" % "jetty" % "6.1.26" % "container",
-"ch.qos.logback" % "logback-classic" % "1.0.6" % "runtime"
-)
-
--- project/ProjectBuild.scala
+++ project/ProjectBuild.scala
@@ -1,11 +1,14 @@
 import sbt._
 import Keys._
 
-object ProjectBuild {
+
+object ProjectBuild extends Build {
+
+  import ProjectBuild.Dependencies._
 
   object Dependencies {
     val scalatest = "org.scalatest" %% "scalatest" % "1.8"
-    val dispatch = "net.databinder.dispatch" %% "dispatch-core" % "0.9.3"
+    val tika = "org.apache.tika" % "tika-core" % "1.2"
   }
 
   val params = Seq(
@@ -14,12 +17,21 @@ object ProjectBuild {
     scalaVersion := "2.9.2",
     javacOptions ++= Seq("-target", "1.6", "-source", "1.6"))
 
-  lazy val projectSettings = Project.defaultSettings ++= params
+  lazy val projectSettings = Project.defaultSettings ++ params
 
   lazy val baseProject = Project(
     id = "strangeloop2012-analytics",
     base = file("."),
-    settings = projectSettings
+    settings = projectSettings,
+    aggregate = Seq(slidesIndexer)
+  )
+
+  lazy val slidesIndexer = Project(
+    id = "slides-indexer",
+    base = file("slides-indexer"),
+    settings = projectSettings ++ Seq(
+      libraryDependencies ++= Seq(tika)
+    )
   )
 
 
